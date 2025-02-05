f3443e5c2a4fc98382973cddf14658ad434cf66d
adelsonsljunior <junioradelsonst@gmail.com>
2025-02-05 15:14:18 -0300

    feat: card registration

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 class Card:
 
-    def __init__(self, id, name, game, rarity, price, stock, sold):
+    def __init__(self, id, game, name, rarity, price, stock, sold=0):  # sold é opcional
         self.id = id
         self.game = game
         self.name = name
         self.rarity = rarity
         self.price = price
         self.stock = stock
-        self.sold = sold
+        self.sold = sold  # Valor padrão é 0
 
     def __str__(self):
         return f"id: {self.id}, game: {self.game}, name: {self.name}, rarity: {self.rarity}, price: {self.price}, stock: {self.stock}, sold: {self.sold}"
 
 
-games = {1: "Yu-Gi-Oh", 2: "Pokémon", 3: "Digimon"}
+# fmt: off
+games = {
+    1: "Yu-Gi-Oh", 
+    2: "Pokémon", 
+    3: "Digimon"
+}
 
-rarities = {1: "Common", 2: "Rare", 3: "Super Rare", 4: "Ultra Rare"}
+rarities = {
+    1: "Common", 
+    2: "Rare", 
+    3: "Super Rare", 
+    4: "Ultra Rare"
+}
+# fmt: on
 
 cards = [
     Card(1, "Red-Eyes Black Dragon", games[1], rarities[4], 100.00, 10, 0),
@@ -29,23 +40,39 @@ cards = [
     Card(9, "WarGreymon", games[3], rarities[4], 300.00, 7, 0),
 ]
 
+
 def display_games():
     print("\n" + "=" * 15)
     for key, value in games.items():
         print(f"{key} - {value}")
     print("=" * 15 + "\n")
 
+
+def display_rarities():
+    print("\n" + "=" * 15)
+    for key, value in rarities.items():
+        print(f"{key} - {value}")
+    print("=" * 15 + "\n")
+
+
 def register_card():
 
-    game = input("Digite o name do game: ")
+    display_games()
+    game = int(input("Digite o jogo da carta: "))
     name = input("Digite o name da carta: ")
-    rarity = input("Digite a rarity da carta: ")
+    display_rarities()
+    rarity = int(input("Digite a rarity da carta: "))
     price = float(input("Digite o preço da carta: "))
     stock = int(input("Digite a quantidade em estoque: "))
 
-    card = Card(name, game, rarity, price, stock)
+    # Gerar um id para a carta
+    card_id = len(cards) + 1
+
+    card = Card(id, name, games[game], rarities[rarity], price, stock)
 
     cards.append(card)
+
+    print("\n[INFO] - Carta cadastrada com sucesso")
 
 
 def list_cards():
@@ -56,7 +83,7 @@ def list_cards():
 
 
 def list_cards_by_game():
-    
+
     display_games()
     game = int(input("Digite o jogo desejado: "))
 
@@ -83,6 +110,7 @@ def menu():
         print("1 - Cadastrar Carta")
         print("2 - Listar Cartas")
         print("3 - Listar Cartas por Jogo")
+        print("4 - Listar Cartas por Raridade")
         print("0 - Sair")
         print("=" * 20 + "\n")
 
@@ -95,6 +123,8 @@ def menu():
                 list_cards()
             case 3:
                 list_cards_by_game()
+            case 4:
+                list_cards_by_rarity()    
             case _:
                 print("Opção inválida")
 
