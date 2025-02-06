dc3f78cf311919b1f85f5fc08506e932303b26e6
adelsonsljunior <junioradelsonst@gmail.com>
2025-02-06 17:12:25 -0300

    dictionaries em um arquivo separado

---
+++ src/dictionaries.py
@@ -0,0 +1,14 @@
+#fmt off
+games = {
+    1: "Yu-Gi-Oh", 
+    2: "Pokémon", 
+    3: "Digimon"
+}
+
+
+rarities = {
+    1: "Comum", 
+    2: "Rara", 
+    3: "Super Rara", 
+    4: "Ultra Rara"
+}
